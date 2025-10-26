f924b7c2f115ba63a92f69cf6d43f88d27a44df5
Cristopher <cristophermatiasmesinamorales@gmail.com>
2025-10-26 18:49:33 -0300

    Se agrega opcion de moficacion de usuario.

--- index.js
+++ index.js
@@ -51,10 +51,18 @@ async function index(){
                 }, 1000);
                 break;
             case '3':
-                console.log('Opcion en desarrollo');
+                console.log('Cargando modificacion de usuarios...')
+                setTimeout(() =>{
+                    console.clear();
+                    modificarUsuario();
+                }, 1000);
                 break;
             case '4':
-                console.log('Opcion en desarrollo');
+                console.log('Opcion para eliminar usuarios no disponible aún...');
+                setTimeout(() =>{
+                    console.clear();
+                    index();
+                }, 1000)
                 break;
             case '5':
                 console.log('Saliendo...');
@@ -86,10 +94,13 @@ async function crearLibro(){
     workbook.sheet(0).cell('D1').value('Email');
 
     workbook.toFileAsync('./usuarios.xlsx').then(()=>{
+        console.log('Estamos creando el libro...');
+        console.log('Por favor espera...');
         console.log('Libro creado');
+        console.log('Redireccionando al menu principal...');
         setTimeout(()=>{
             index();
-        }, 500);
+        }, 2000);
     })
 
     existeLibro = true;
@@ -100,39 +111,45 @@ async function crearLibro(){
 async function ingresarDatos() {
 
 
-    const workbook = await XlsxPopulate.fromFileAsync('./usuarios.xlsx');
+    try{
+        const workbook = await XlsxPopulate.fromFileAsync('./usuarios.xlsx');
 
-    const total = workbook.sheet(0).usedRange().value().length - 1;
+        const total = workbook.sheet(0).usedRange().value().length - 1;
 
-    console.clear();
-    console.log('Esta es una base datos que almacena información de usuarios.');
-    console.log();
-    
-    rl.question('¿Cuantos usuarios quieres ingresar?: ', (id) =>{
-        let contador = 1;
-        function agregarUsuario(){
-            if(contador <= parseInt(id)){
-                rl.question(`Ingrese el nombre del usuario ${total + contador}: `, (nombre) => {
-                    rl.question(`Ingrese el apellido del usuario ${total + contador}: `, (apellido) => {
-                        workbook.sheet(0).cell(`A${total + contador + 1}`).value(total + contador);
-                        workbook.sheet(0).cell(`B${total + contador + 1}`).value(nombre);
-                        workbook.sheet(0).cell(`C${total + contador + 1}`).value(apellido);
-                        workbook.sheet(0).cell(`D${total + contador + 1}`).formula(`CONCATENATE(B${total + contador + 1},".",C${total + contador + 1},"@correo.com")`);
-                        console.clear();
-                        contador++;
-                        agregarUsuario();
+        console.clear();
+        console.log('Esta es una base datos que almacena información de usuarios.');
+        
+        rl.question('¿Cuantos usuarios quieres ingresar?: ', (id) =>{
+            let contador = 1;
+            function agregarUsuario(){
+                if(contador <= parseInt(id)){
+                    rl.question(`Ingrese el nombre del usuario ${total + contador}: `, (nombre) => {
+                        rl.question(`Ingrese el apellido del usuario ${total + contador}: `, (apellido) => {
+                            workbook.sheet(0).cell(`A${total + contador + 1}`).value(total + contador);
+                            workbook.sheet(0).cell(`B${total + contador + 1}`).value(nombre);
+                            workbook.sheet(0).cell(`C${total + contador + 1}`).value(apellido);
+                            workbook.sheet(0).cell(`D${total + contador + 1}`).formula(`CONCATENATE(B${total + contador + 1},".",C${total + contador + 1},"@correo.com")`);
+                            console.clear();
+                            contador++;
+                            agregarUsuario();
+                        });
                     });
-                });
-            } else {
-                workbook.toFileAsync('./usuarios.xlsx').then(() => {
-                    console.log('Datos guardados en usuarios.xlsx');
-                    index();
-                });
+                } else {
+                    workbook.toFileAsync('./usuarios.xlsx').then(() => {
+                        console.log('Datos guardados en usuarios.xlsx');
+                        index();
+                    });
+                }
             }
-        }
-        agregarUsuario();
-    })
+            agregarUsuario();
+        })
 
+    } catch (err){
+        crearLibro();
+        setTimeout(() => {
+            index();
+        }, 3000);
+    }
 
 }
 
@@ -146,4 +163,27 @@ async function mostrarUsuarios(){
             index();
         }
     })
+}
+
+async function modificarUsuario(){
+    const workbook = await XlsxPopulate.fromFileAsync('./usuarios.xlsx');
+    const datos = workbook.sheet(0).usedRange().value();
+    console.table(datos);
+
+    rl.question('¿Que ID de usuario quieres modificar?: \n', (id) =>{
+        const fila = parseInt(id) + 1;
+        rl.question('Ingrese el nombre nuevo: ', (nombre)=>{
+            rl.question('Ingrese el apellido nuevo: ', (apellido) =>{
+                workbook.sheet(0).cell(`B${fila}`).value(nombre);
+                workbook.sheet(0).cell(`C${fila}`).value(apellido);
+                workbook.sheet(0).cell(`D${fila}`).formula(`CONCATENATE(B${fila},".",C${fila},"@correo.com")`);
+                workbook.toFileAsync('./usuarios.xlsx').then(() => {
+                    console.log('Datos modificados en usuarios.xlsx');
+                    index();
+                });
+            });
+        });
+    });
+
+
 }
